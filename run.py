daa1e218ca805b658060c2e2e250af5df456b680
seanpatton1 Patton <seanpatton1@outlook.com>
2024-05-21 20:17:29 +0000

    removed code - redoing

--- run.py
+++ run.py
@@ -3,15 +3,3 @@ from words import words
 import string
 
 
-def get_valid_words(words):
-    word = random.choice(words) #radomly chooses something from the list
-    while '-' in word or ' ' in word:
-        word = random.choice(words)
-        
-    return word.upper()
-
-def hangman():
-    word = get_valid_words(words)
-    word_letters = set(word) # letters in the word
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set() # what the user has guessed
